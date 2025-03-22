f8616d97732d199148c5af937c9be5953c087abb
eranmazuz <mazuzeran@gmail.com>
2025-03-22 10:21:47 +0200

    Add the DataContext to the app.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using HomeAssignmentDotNet8EntityFramework.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var myAllowAllOrigins = "_myAllowAllOrigins";
 
@@ -7,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddCors(options =>
 {
